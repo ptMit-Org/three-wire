095c03083cec650a015ee8c7815d1987f397999a
Ismail <ismailisims1@gmail.com>
2026-04-03 00:51:14 +0530

    done

--- js/intro_data.js
+++ js/intro_data.js
@@ -21,9 +21,9 @@ export const INTRO_CONTENT = {
     
     // Student List
     students: [
-        "Vignesh N - 2023507011",
-        "Keshav Narayan L M - 2023507018",
-        "Akilan K - 2023507020",
-        "Aswin G - 2023507044"
+        { name: "Vignesh N", roll: "2023507011", image: "../studentimages/2023507011.jpg" },
+        { name: "Keshav Narayan L M", roll: "2023507018", image: "../studentimages/2023507018.jpg" },
+        { name: "Akilan K", roll: "2023507020", image: "../studentimages/2023507020.jpg" },
+        { name: "Aswin G", roll: "2023507044", image: "../studentimages/2023507044.jpg" }
     ]
 };
